2c1765d3a3556dbbad7df2f50d08eb3832b919cf
SafarbatiSami <samsafarbati@hotmail.com>
2024-04-22 18:03:06 +0200

    removed useless imports

--- wrapper.py
+++ wrapper.py
@@ -1,13 +1,11 @@
 import sys
-import os
-import shutil
+
 import subprocess
 from subprocess import call
-from cytomine.models import Job
+
 from biaflows import CLASS_OBJSEG, CLASS_SPTCNT, CLASS_PIXCLA, CLASS_TRETRC, CLASS_LOOTRC, CLASS_OBJDET, CLASS_PRTTRK, CLASS_OBJTRK
 from biaflows.helpers import BiaflowsJob, prepare_data, upload_data, upload_metrics, get_discipline
-import time
-import shutil
+
 # Assuming biom3d has relevant classes/functions you need to import
 
 
@@ -45,7 +43,7 @@ def main(argv):
             sys.exit(1)
                 # 5. Pipeline finished
 
-        bj.job.update(progress=100, status=Job.TERMINATED, status_comment="Finished.")
+        
 
 if __name__ == "__main__":
     main(sys.argv[1:])
